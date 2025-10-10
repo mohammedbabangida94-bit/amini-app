06afd39edbd7b1f989458c23148618027a758008
mohammedbabangida94-bit <mohammedbabangida94@gmail.com>
2025-10-10 17:36:45 +0100

    Update welcome message text

--- amini-docker-project/server.js
+++ amini-docker-project/server.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const express = require('express');
 
 // Constants
 const PORT = process.env.PORT || 3000;
-const HOST = '0.0.0.0'; // This is important for Docker
+const HOST = '0.0.0.0'; 
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from your Amini App!</h1><p>It is currently running inside a Docker container.</p>');
+  // This is the updated message
+  res.send('<h1>Hello from your Amini App!</h1><p>It is now running on Render.</p>');
 });
 
 app.listen(PORT, HOST, () => {
   console.log(`Amini app is running on http://${HOST}:${PORT}`);
-});      
+});
