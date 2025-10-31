e4027b701e5a188d79a03a61f0e9c83b40a21bf5
mohammedbabangida94-bit <mohammedbabangida94@gmail.com>
2025-10-31 18:59:36 +0100

    Final: Corrected dotenv initialization syntax

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // =================================================================
 // 1. IMPORTS
 // =================================================================
-require('dotenv').config();
+require ('dotenv').config();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const express = require('express');
